eba0367e762c2c554ade6e746f7babb224c94eb6
svnh <sekunovsky@gmail.com>
2015-04-14 18:12:21 -0700

    egghead hello

---
+++ egghead/helloworld/hello.jsx
@@ -0,0 +1,30 @@
+var App = React.createClass({
+  getDefaultProps:function(){
+    return {
+      greeting: 'Hello',
+      place: 'Venus'
+    }
+  },
+  propTypes: {
+    greeting: React.PropTypes.string.isRequired,
+    place: React.PropTypes.string.isRequired
+  },
+  render:function(){
+    var greeting = this.props.greeting;
+
+    // Two elements cannot be rendered without an encasing parent node
+    // because this is the same as returning two functions
+    // when it is compiled to Javascript
+
+    // Properties are rendered through interpolation
+    return (
+      <div>
+        <h2>{ greeting }</h2>
+        <h1>{ this.props.place }</h1>
+      </div>
+    )
+  }
+});
+
+// Render app component on body
+React.render(<App place="World" />, document.body);
